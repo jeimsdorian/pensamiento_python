b732e1605631ee5dc103bb2fca7ae9bc21bda78d
Jeims Dorian <jeimsdorianm@gmail.com>
2023-09-19 12:03:31 -0400

    busqueda binaria mas bonita

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -1,14 +1,14 @@
-objetivo = int(input('escoge un numero:'))
-epsilon = 0.000000000000001
+objetivo = int(input('escoge un numero: '))
+epsilon = 0.01
 bajo = 0.0
 alto = max(1.0, objetivo)
 respuesta = (alto + bajo ) /2
 
-while abs(respuesta**2 -objetivo) >= epsilon:
+while abs(respuesta**2 - objetivo) >= epsilon:
     print(f'bajo={bajo} alto{alto} respuesta{respuesta}')
     if  respuesta**2 < objetivo:
         bajo = respuesta
     else:
         alto = respuesta
-    respuesta = (alto + bajo) / 2
+    respuesta = (alto + bajo) /2
 print(f'la raiz de {objetivo} es {respuesta}')
